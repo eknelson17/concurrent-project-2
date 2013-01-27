59ed30e246e49775948f9ce42688f1b02a2392a0
Emma Nelson <eknelson17@gmail.com>
2013-01-27 18:59:30 -0500

    DocScanner is "done", Controller and tsa are in progress

--- DocScanner.scala
+++ DocScanner.scala
@@ -1,17 +1,26 @@
+// Author: Emma Nelson
+
 import akka.actor.*
 
-class DocScanner(val nLines : Int, var queues : List) extends Actor {
+// Immutable message sent to the queue with the passenger
+case class ToLine() { passenger : val }
+
+// Gets a passenger from the Controller, decides whether or not the
+// passenger's documentation is valid (80% chance it is) and sends them
+// on to the next queue. If not, they are turned away.
+class DocScanner(val nLines : Int, val queues : List) extends Actor {
 	val numLines = nLines
-	val nextLine = 0
-	var random = new Random()
+	var nextLine = 0
+	val random = new Random()
 	
 	def receive = {
-		case GetPassenger =>
+		case GetPassenger(passenger) =>
 			if(random.nextInt(100) > 20) {
-				queues[nextLine] ! ToLine(GetPassenger.passenger)
-				println("Passenger " + GetPassenger.passenger + " is sent to queue " + nextLine + " by the Doc Scanner.")
+				queues[nextLine] ! ToLine(passenger)
+				println("Passenger " + passenger + " is sent to queue " + nextLine + " by the Doc Scanner.")
+				nextLine = (nextLine = nextLine + 1) % numLines
 			} else {
-				println("Passenger " + GetPassenger.passenger + " has invalid documentation.")
+				println("Passenger " + passenger + " has invalid documentation.")
 			}
 	}
 }
--- tsa.scala
+++ tsa.scala
@@ -1,8 +1,30 @@
+// Author: Emma Nelson
+
+// Sent to the Controller to tell it to create passengers
+case object SendPassengers
+
 // Main file - starts everything, controls time,
 // says when to send bursts of passengers, and
 // tells the controller to close everything
 object tsa extends Any {
+	var NLINES = 5;
+
 	def main(arg: Array[String]) = {
-		
+		var queues = List.empty[ActorRef]
+
+		// Create all the queues and add them to a list to be sent to the DocScanner
+		var queue
+		for(i <- 1 to NLINES) {
+			// TODO fix the following line to be correct for the Queue class
+			queue = Actor.actorOf[Queue].start()
+			queues = queue :: queues
+		}
+
+		// Create and start the DocScanner
+		val docSanner = Actor.actorOf(DocScanner(NLINES, queues)).start()
+
+		// Create and start the Controller
+		// TODO add other needed params
+		val controller = Actor.actorOf(Controller(docSanner)).start()
 	}
 }
