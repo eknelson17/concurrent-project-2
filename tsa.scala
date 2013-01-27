4808d9de14f4ecd8b508aa02d1e402a6c012b019
Emma Nelson <eknelson17@gmail.com>
2013-01-27 17:39:27 -0500

    Added the first file

---
+++ tsa.scala
@@ -0,0 +1,8 @@
+// Main file - starts everything, controls time,
+// says when to send bursts of passengers, and
+// tells the controller to close everything
+object tsa extends Any {
+	def main(arg: Array[String]) = {
+		
+	}
+}
