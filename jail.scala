aa4c444fc2ecb3516b3acb454275cfa4a62f76d7
Dan Lavoie <danlavoi@gmail.com>
2013-01-27 18:23:57 -0500

    Filled in the jail class
    I haven't tested the code so I don't know how Scala-kosher it is.

--- jail.scala
+++ jail.scala
@@ -1,8 +1,21 @@
-import akka.actor.TypedActor
+import akka.actor.*
 
 //The Jail keeps a list of immutable passenger objects it is sent.
 //At the end of the day, it receives a message to put its passengers
 //in permanent lockup, and purges all of its passengers.
-object jail extends TypedActor {
-	
+class Jail() extends Actor {
+	var passengers = new List()
+	def receive = {
+		case Prisoner =>
+			passengers = passengers ++ Prisoner.passenger
+			println("Passenger " + GetPassenger.passenger + " is sent to jail by a security station")
+		case Close =>
+			while(!passengers.isEmpty) {
+				//While passengers are left, slice them out one at a time
+				//and print who we've removed
+				println("Passenger " + passengers.head + " is being sent to permanent lockup.")
+				passengers = passengers.slice(1,passengers.length)
+			}
+			println("All passengers in permanent lockup. Jail is closing for the day.")
+	}
 }
