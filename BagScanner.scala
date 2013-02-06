ac2a9e638c355246e49fd5e05d3348885f0dd4fc
Maddison Hickson <mjhickson@gmai.com>
2013-02-06 11:42:50 -0500

    added comments

--- BagScanner.scala
+++ BagScanner.scala
@@ -4,6 +4,8 @@ import akka.actor.Actor
 import akka.actor.ActorRef
 import scala.util.Random
 
+// Decides whether a passenger's bag passes or fails(20% chance they fail) 
+// the baggage scan and then relays this information to the SecurityStation. 
 class BagScanner(val nLines : Int, val securityStation : ActorRef) extends Actor {
 	var hasPassed = true
 	val random = new Random()
@@ -12,14 +14,17 @@ class BagScanner(val nLines : Int, val securityStation : ActorRef) extends Actor
 		securityStation ! SendBagScanner(self)
 	}
 
+	//recieves a bag from DocScanner to scan
 	def receive = {	
 		case GetPassenger(bag) =>
+			//decides whether the bag fails or passes the scan
 			if(random.nextInt(5) == 1) {
 				hasPassed = false
 				println("The bag belonging to Passenger " + bag + " has failed inspection.")
 			} else {
 				println("The bag belonging to Passenger " + bag + " has passed inspection.")
 			}
+			//sends a tuple with the Passenger and whether or not the bag passed the scan
 			securityStation ! BagToSecurityStation(new Tuple2(bag : Int, hasPassed : Boolean))
 			println("The bag belonging to Passenger " + bag + " was sent to the Security Station.")
 	}
