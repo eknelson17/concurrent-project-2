a3d378f3482f1618ed0e0a26bd703dc64a7eeb16
Maddison Hickson <mjhickson@gmai.com>
2013-02-06 11:55:50 -0500

    more comments

--- DocScanner.scala
+++ DocScanner.scala
@@ -24,8 +24,10 @@ class DocScanner(val numLines : Int) extends Actor {
 		case GetPassenger(passenger) =>
 			//decides whether a passenger fails the document check
 			if(random.nextInt(100) > 20) {
+				//sends the passenger to a body scanner
 				bodyScanners.get(nextLine).head ! ToLine(passenger)
 				println("Passenger " + passenger + " is sent to wait for Body Scanner " + nextLine + " by the Doc Scanner.")
+				//sends the passenger's bag to the bag scanner in the same line
 				bagScanners.get(nextLine).head ! ToLine(passenger)
 				println("Passenger " + passenger + "'s bag is sent to wait for Bag Scanner " + nextLine + " by the Doc Scanner.")
 				nextLine = (nextLine + 1) % numLines
