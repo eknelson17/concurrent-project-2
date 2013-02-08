d53376284be7331cecd3f701661461ded3215a4e
Maddison Hickson <mjhickson@gmai.com>
2013-02-08 16:40:27 -0500

    fixed awful merge problem

--- BagScanner.scala
+++ BagScanner.scala
@@ -32,42 +32,6 @@ class BagScanner(val id : Int, val securityStation : ActorRef) extends Actor {
 			securityStation ! BagToSecurityStation(new Tuple2(bag : Int, hasPassed : Boolean))
 			println("The bag belonging to Passenger " + bag + " was sent to the Security Station by the BagScanner.")
 	}
-}
-	override def postStop {
-		securityStation ! ScannerClosed(id)
-	}
-// Author: Maddison Hickson
-// Edited By: Emma Nelson
-import akka.actor.Actor
-import akka.actor.ActorRef
-import scala.util.Random
-
-// Gets a bag from DocScanner Decides whether a passenger's bag passes or 
-// fails(20% chance they fail) the baggage scan and then relays this 
-// information to the SecurityStation. 
-class BagScanner(val id : Int, val securityStation : ActorRef) extends Actor {
-	var hasPassed = true
-	val random = new Random()
-
-	override def preStart = {
-		securityStation ! SendBagScanner(self)
-	}
-
-	def receive = {	
-		//recieves a bag from DocScanner to scan
-		case ToLine(bag) =>
-			//decides whether the bag fails or passes the scan
-			if(random.nextInt(5) == 1) {
-				hasPassed = false
-				println("The bag belonging to Passenger " + bag + " has failed inspection.")
-			} else {
-				hasPassed = true
-				println("The bag belonging to Passenger " + bag + " has passed inspection.")
-			}
-			//sends a tuple with the Passenger and whether or not the bag passed the scan
-			securityStation ! BagToSecurityStation(new Tuple2(bag : Int, hasPassed : Boolean))
-			println("The bag belonging to Passenger " + bag + " was sent to the Security Station by the BagScanner.")
-	}
 
 	override def postStop {
 		securityStation ! ScannerClosed(id)
